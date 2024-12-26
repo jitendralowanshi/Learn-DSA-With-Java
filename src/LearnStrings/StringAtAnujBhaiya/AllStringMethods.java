d0b82d42058b5288bd5508a4d2f94758e321d965
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2024-12-26 22:21:58 +0530

    today learn String and his all methods

---
+++ src/LearnStrings/StringAtAnujBhaiya/AllStringMethods.java
@@ -0,0 +1,83 @@
+package LearnStrings.StringAtAnujBhaiya;
+
+public class AllStringMethods {
+    public static void main(String[] args) {
+        String name = "jiteNDra";
+        String lastName = "LowANshi";
+
+//       1 FIRST METHOD.  toUpperCase()
+        String toUpperCase = name.toUpperCase();
+        System.out.println(toUpperCase);
+
+//       2 SECOND METHOD.   toLowerCase()
+        String toLowerCase = lastName.toLowerCase();
+        System.out.println(toLowerCase);
+
+
+//        3 THIRD METHOD   trim()
+//        this trim method only trim spaces both sides , and delet the spaces both sides
+        String s = "    jitendra       ";
+        String s1 = s.trim();
+        System.out.println(s1);
+
+
+//        4 FOURTH METHOD .  startsWith()
+        System.out.println("startsWith = "+name.startsWith("jit"));
+
+
+//        5 FOURTH METHOD .  endsWith()
+        System.out.println("endsWith = "+name.endsWith("ra"));
+
+
+//        6 SIXTH METHOD    equals()
+        System.out.println(name.equals(lastName));
+
+
+//        7 SEVENTH METHOD  equalsIgnoreCase()
+        System.out.println("jitendra".equalsIgnoreCase("jiTEndra"));
+
+
+//        MORE USEFUL METHODS IN DSA
+
+//        8 EIGTH METHOD  charAt()
+//        this method return any character in a string
+        System.out.println(name.charAt(0));
+
+
+//        9 NINETH METHOD   valueOf()
+//        this method change any integer number into Strings
+        int age = 123;
+        String stringAge = String.valueOf(age);
+        System.out.println(age);
+        System.out.println(stringAge+6);
+
+
+//        10 TENTH METHOD       replace()
+//        11 ELEVANTH METHOD    contains()
+//        12 TWELTH METHOD      substringn();
+//        13 THIRTEEN METHOD    split()
+
+
+//        14 FOURTEEN TH METHOD
+//        toCharArray convert String into array
+        String  color = "Brown";
+        char letters[] = color.toCharArray();
+        for (char let: letters) {
+            System.out.println(let);
+        }
+//        System.out.println(letters[1]);
+
+
+//        15 FIFTEEN TH METHOD  isEmpty()
+        String animal = "     ";
+
+        if (animal.isEmpty()) {
+            System.out.println("empty");
+        }
+        else if (animal.isBlank()) {
+            System.out.println("blank");
+        }
+
+
+    }
+}
---
+++ src/LearnStrings/StringAtAnujBhaiya/StringBasics.java
@@ -0,0 +1,55 @@
+package LearnStrings.StringAtAnujBhaiya;
+
+import java.sql.SQLOutput;
+
+public class StringBasics {
+    public static void main(String[] args) {
+//        using string literals
+        String name = "Jitendra";
+        String sameName = "Jitendra";
+
+//        using new akeywords
+        String newName = new String("Jitendra");
+
+        System.out.println(name);
+        System.out.println(sameName);
+        System.out.println(newName);
+
+//it is also, == check refrences like name & sameName point same space
+        if (name == sameName) {
+            System.out.println("both are same ");
+        } else {
+            System.out.println("both are not same");
+        }
+
+//it is check only,== check refrences  like name & newname point diffenent space
+        if (name == newName) {
+            System.out.println("Both are same");
+        } else {
+            System.out.println("both are not same");
+        }
+
+//  it is check values are equals or not
+//  java is a casesensative language jitendra & JITENDRA  are different values
+//   if we can ignore the tho cases jitendra & JITENDRA then use {  equalsIgnoreCAse()  } methid
+        if(name.equals(sameName)) {
+            System.out.println("name and samename are equls ");
+        } else {
+            System.out.println("name and sameName are Not equals ");
+        }
+
+        if (name.equals(newName)) {
+            System.out.println("name and newName are equals ");
+        } else {
+            System.out.println("name and newName are Not equals ");
+        }
+
+//  this method ignore the cases and ignore casesensitive values
+        String a = "name";
+        String b = "NAME";
+
+        if (a.equalsIgnoreCase(b)){
+            System.out.println("a and b are same values ");
+        }
+    }
+}
