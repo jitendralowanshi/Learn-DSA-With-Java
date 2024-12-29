458b3b1c9a1998daa2391b9578a4bb6eada09046
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2024-12-29 22:26:28 +0530

    add

--- src/LearnArrays/ReturnArrayInMethod.java
+++ src/LearnArrays/ReturnArrayInMethod.java
@@ -2,10 +2,25 @@ package LearnArrays;
 
 import java.util.Scanner;
 
-public class ArrayReturnInMethod {
-    public static int[] sortArr(int[] arr) {
+public class ReturnArrayInMethod {
+
+    public static int[] sortArrDece(int[] arr) {
+
         int n = arr.length;
-        int[] abc = new int[n];
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (arr[i] < arr[j]) {
+                    int swap = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = swap;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static int[] sortArr(int[] arr) {
 
         for (int i=0; i<arr.length; i++) {
             for (int j=i+1; j<arr.length; j++) {
@@ -32,12 +47,21 @@ public class ArrayReturnInMethod {
             arr[i] = sc.nextInt();
         }
 
+//        CALL SORTARR FUNCTION
         sortArr(arr);
-        System.out.println("Your sorted array is ");
+        System.out.println("Your sorted Assending array is ");
         for (int ans: arr){
             System.out.print(ans+" ");
         }
+        System.out.println();
 
+
+//        CALL SORDARRDECENDING ARRAY
+        sortArrDece(arr);
+        System.out.println("Your sorted Decending array is ");
+        for (int dec: arr){
+            System.out.print(dec+" ");
+        }
 
 
     }
