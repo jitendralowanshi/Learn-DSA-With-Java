2cab6d3e6f6ddd820c3677c1b2826ba0198b61ba
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-02-07 23:38:47 +0530

    binarysearch using recurive function

--- src/Algorithms/Searching/binarsySearchUsingRecursion.java
+++ src/Algorithms/Searching/binarsySearchUsingRecursion.java
@@ -1,4 +1,30 @@
 package Algorithms.Searching;
 
 public class binarsySearchUsingRecursion {
+    public static int recursivBinarySerach(int[] arr, int target, int start, int end) {
+
+        int mid = start + (end-start)/2;
+        if(target > arr[mid]) {
+            return recursivBinarySerach(arr, target, mid+1, end);
+        }
+        else if (target < arr[mid]) {
+            return recursivBinarySerach(arr,target,start,mid-1);
+        }
+        else {
+            return mid;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int target = 6;
+
+        int start = 0;
+        int end = arr.length-1;
+
+        int ans = recursivBinarySerach(arr, target,start,end);
+        System.out.println(ans);
+    }
 }
