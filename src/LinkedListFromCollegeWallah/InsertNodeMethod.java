a66021c9573158d36b88f201ad3c0b76be993cee
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-02-25 15:15:25 +0530

    add nes method insertNodeAt index value

---
+++ src/LinkedListFromCollegeWallah/InsertNodeMethod.java
@@ -0,0 +1,30 @@
+package LinkedListFromCollegeWallah;
+
+public class InsertNodeMethod {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node (int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class Linkedlist {
+        Node head = null;
+        Node tail = null;
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+
+
+
+    }
+}
