da0a1376c1de5b868afc3e91860c169a951565c4
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-02-28 22:41:06 +0530

    reverse a stack

--- src/Learn_Stack/FromApnaCollege/PushAtBottomOfStack.java
+++ src/Learn_Stack/FromApnaCollege/PushAtBottomOfStack.java
@@ -1,4 +1,48 @@
 package Learn_Stack.FromApnaCollege;
 
+import java.util.Stack;
+
 public class PushAtBottomOfStack {
+
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s,data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s,top);
+    }
+
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        System.out.print(s+" ");
+        System.out.println();
+
+        reverseStack(s);
+        System.out.print(s+" ");
+        System.out.println();
+
+        while(!s.isEmpty()) {
+            System.out.print(s.peek()+" ");
+            s.pop();
+        }
+
+
+    }
 }
